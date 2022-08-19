072a93ba6a70f26a91b307e63b778c4d6ae53967
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-19 17:28:19 -0600

    Vehicle method updates

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,14 @@
 class Vehicle < ActiveRecord::Base 
   belongs_to :user 
   has_many :tasks
+
+  def self.get_motorcycles
+    motos = Vehicle.where(vehicle_type: "Motorcycle")
+    motos.to_json
+  end
+
+  def self.get_cars
+    cars = Vehicle.where(vehicle_type: "Car")
+    cars.to_json 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "🌱 Seeding vrooms..."
 
 User.create(name: "Riley", state: "Colorado", city: "Denver")
 Vehicle.create(vehicle_type: "Motorcycle", make: "Suzuki", model: "SV650", year: 2001, nickname: "Squirtle", user_id: 1, owned_since: 2020)
-Vehicle.create(vehicle_type: "Truck", make: "Toyota", model: "4Runner", year: 2000, nickname: "Kaylee", user_id: 1, owned_since: 2018)
+Vehicle.create(vehicle_type: "Car", make: "Toyota", model: "4Runner", year: 2000, nickname: "Kaylee", user_id: 1, owned_since: 2018)
 Task.create(name: "Oil Change", description: "Change the oil!", date_due: 2022, last_done: 2021, price: 0, vehicle_id: 1)
 Task.create(name: "Rotate tires", description: "Rotate the tires!", date_due: 2023, last_done: 2022, price: 0, vehicle_id: 2)
 
