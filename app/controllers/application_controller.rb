37a827d4db2e58bb2e485c7f842ccd91a780f03a
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-20 05:16:22 -0600

    model updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,45 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  get "/users" do 
-    User.all.to_json 
+  get '/users' do
+    User.all.to_json
   end
 
-  get "/users/:id" do 
+  get '/users/:id' do
     user = User.find(params[:id])
     user.to_json(include: :vehicles)
   end
 
-  get "/vehicles" do 
+  delete '/users/:id' do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
+  end
+
+  get '/vehicles' do
     Vehicle.all.to_json
   end
 
-  get "/vehicles/:id" do 
+  get '/vehicles/:id' do
     vehicle = Vehicle.find(params[:id])
     vehicle.to_json(include: :tasks)
   end
 
-  get "/tasks" do
+  delete '/vehicles/:id' do
+    vehicle = Vehicle.find(params[:id])
+    vehicle.destroy
+    vehicle.to_json
+  end
+
+  get '/tasks' do
     Task.all.to_json
+  end
+
+  patch '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.update(
+      completed: params[:completed]
+    )
+    task.to_json
   end
 end
