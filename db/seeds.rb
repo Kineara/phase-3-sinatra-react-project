62905f4aeb91d805c34219c83f8973378bd006a5
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-21 01:14:43 -0600

    seed fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@ Vehicle.create(vehicle_type: "Motorcycle", make: "Suzuki", model: "SV650", year:
 Vehicle.create(vehicle_type: "Motorcycle", make: "Suzuki", model: "GSXR 600", year: 2005, nickname: "GSXRAWR", user_id: 1, owned_since: 2022)
 Vehicle.create(vehicle_type: "Car", make: "Toyota", model: "4Runner", year: 2000, nickname: "Kaylee", user_id: 1, owned_since: 2016)
 Task.create(name: "Oil Change", description: "Change the oil!", date_due: 2022, category: "Oil Changes", vehicle_id: 1)
-Task.create(name: "Rotate tires", description: "Rotate the tires!", date_due: 2023, category: "Wheels", vehicle_id: 2)
+Task.create(name: "Rotate tires", description: "Rotate the tires!", date_due: 2023, category: "Wheels", vehicle_id: 3)
 
 puts "✅ Vrooms ready!"
